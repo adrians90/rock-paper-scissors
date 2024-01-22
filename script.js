a9321e2fe5be8a29e5c820b5835b6d2f8814bc4b
Adrian <adriansandu9090@gmail.com>
2024-01-22 11:37:21 +0000

    getComputerChoice function generates a random choice for the computer

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hi there");
+
+// create a getComputerChoice function to randomly select rock paper or scissors
+// get inpu† from user - choices are : Rock, Paper, Scissors
+//store inpu† in a variable
+// make computer choose randomly between rock paper and scissors and return the resul
+// play a single round by writing a function that has playerSelection and computerSelection
+// as parameters.
+// compare the input to declare winner
+
+let choices = ["rock", "paper", "scissors"];
+let random = Math.floor(Math.random() * choices.length);
+choice = choices[random];
+
+function getComputerChoice(choice) {
+    return "Computer choose: " + choice;
+}
+getComputerChoice();
+console.log(getComputerChoice(choice))
+
+
